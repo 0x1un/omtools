980f82ca42187161f576237f07e5f5ee7e7b2ce1
aumujun <aumujun@gmail.com>
2020-06-25 23:40:08 +0800

    增加过滤ip功能

--- list_host.go
+++ list_host.go
@@ -6,31 +6,59 @@ import (
 	"zbxtools/go-zabbix"
 )
 
+func (z *ZbxTool) getAnyHostID() ([]string, error) {
+	params := zabbix.HostGetParams{}
+	params.OutputFields = []string{"hostid"}
+	hosts, err := z.session.GetHosts(params)
+	if err != nil {
+		return nil, err
+	}
+	ids := []string{}
+	for _, host := range hosts {
+		if host.HostID == "" {
+			continue
+		}
+		ids = append(ids, host.HostID)
+	}
+	return ids, nil
+}
+
 // @params key: 模糊搜索的关键字
-func (z *ZbxTool) ListHostID(key string) (map[string]string, error) {
+func (z *ZbxTool) ListHostID(key string) error {
 	hostParams := zabbix.HostGetParams{}
 	hostParams.OutputFields = []string{"hostid", "name"}
 
 	hosts, err := z.session.GetHosts(hostParams)
 	if err != nil {
-		return nil, fmt.Errorf("Error getting Hosts: %v", err)
+		return fmt.Errorf("Error getting Hosts: %v", err)
 	}
 
 	if len(hosts) == 0 {
-		return nil, fmt.Errorf("No Hosts found")
+		return fmt.Errorf("No Hosts found")
 	}
-	hostIDS := make(map[string]string, len(hosts))
+
+	p2 := zabbix.HostInterfaceGetParams{}
 	for _, host := range hosts {
 		if len(host.HostID) == 0 {
 			continue
 		}
+		ip := func(id string) string {
+
+			p2.Hostids = []string{host.HostID}
+			p2.Output = []string{"ip"}
+			ifres, err := z.session.HostInterfaceGet(p2)
+			if err != nil {
+				return ""
+			}
+			return ifres[0].IP
+		}
 		if len(key) == 0 {
-			hostIDS[host.HostID] = host.DisplayName
+			println(host.HostID, host.DisplayName+"\t"+ip(host.HostID))
 			continue
 		}
 		if strings.Contains(host.DisplayName, key) {
-			hostIDS[host.HostID] = host.DisplayName
+			println(host.HostID, host.DisplayName+"\t"+ip(host.HostID))
 		}
 	}
-	return hostIDS, nil
+	return nil
 }
