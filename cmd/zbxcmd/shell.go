214345ece60b4424033012cb16980ad4283d32c5
aumujun <aumujun@gmail.com>
2020-06-27 00:03:49 +0800

    move complete function to shellCmd

--- cmd/zbxcmd/shell.go
+++ cmd/zbxcmd/shell.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"strconv"
 	"strings"
@@ -25,6 +26,85 @@ import (
 
 	"github.com/chzyer/readline"
 	"github.com/spf13/cobra"
+)
+
+
+// shellCmd represents the shell command
+var shellCmd = &cobra.Command{
+	Use:   "shell",
+	Short: "交互模式",
+	Long:  `进入交互模式`,
+	Run:   shellcmd,
+}
+
+func init() {
+	rootCmd.AddCommand(shellCmd)
+}
+
+func usage(w io.Writer) {
+	_, _ = io.WriteString(w, "commands:\n")
+	_, _ = io.WriteString(w, completer.Tree("    "))
+}
+
+func filterInput(r rune) (rune, bool) {
+	switch r {
+	// block CtrlZ feature
+	case readline.CharCtrlZ:
+		return r, false
+	}
+	return r, true
+}
+
+// Function constructor - constructs new function for listing given directory
+func listFiles(path string) func(string) []string {
+	return func(line string) []string {
+		names := make([]string, 0)
+		files, _ := ioutil.ReadDir(path)
+		for _, f := range files {
+			names = append(names, f.Name())
+		}
+		return names
+	}
+}
+
+var completer = readline.NewPrefixCompleter(
+	readline.PcItem("query",
+		readline.PcItem("host", readline.PcItem("by")),
+		readline.PcItem("tpl", readline.PcItem("by")),
+		readline.PcItem("graph", readline.PcItem("by")),
+	),
+	readline.PcItem("list",
+		readline.PcItem("host"),
+	),
+	readline.PcItem("mode",
+		readline.PcItem("vi"),
+		readline.PcItem("emacs"),
+	),
+	readline.PcItem("login"),
+	readline.PcItem("say",
+		readline.PcItemDynamic(listFiles("./"),
+			readline.PcItem("with",
+				readline.PcItem("following"),
+				readline.PcItem("items"),
+			),
+		),
+		readline.PcItem("hello"),
+		readline.PcItem("bye"),
+	),
+	readline.PcItem("setprompt"),
+	readline.PcItem("setpassword"),
+	readline.PcItem("bye"),
+	readline.PcItem("help"),
+	readline.PcItem("go",
+		readline.PcItem("build", readline.PcItem("-o"), readline.PcItem("-v")),
+		readline.PcItem("install",
+			readline.PcItem("-v"),
+			readline.PcItem("-vv"),
+			readline.PcItem("-vvv"),
+		),
+		readline.PcItem("test"),
+	),
+	readline.PcItem("sleep"),
 )
 
 func shellcmd(cmd *cobra.Command, args []string) {
@@ -99,16 +179,4 @@ func shellcmd(cmd *cobra.Command, args []string) {
 		}
 	}
 exit:
-}
-
-// shellCmd represents the shell command
-var shellCmd = &cobra.Command{
-	Use:   "shell",
-	Short: "交互模式",
-	Long:  `进入交互模式`,
-	Run:   shellcmd,
-}
-
-func init() {
-	rootCmd.AddCommand(shellCmd)
 }
