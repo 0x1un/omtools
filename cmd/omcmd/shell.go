b364b984d17b35829de52b78fa108f90c0b64ed2
0x1un <aumujun@gmail.com>
2020-07-15 22:11:00 +0800

    分类

--- cmd/omcmd/shell.go
+++ cmd/omcmd/shell.go
@@ -82,6 +82,7 @@ func shellcmd(cmd *cobra.Command, args []string) {
 	})
 
 	log.SetOutput(l.Stderr())
+	log.SetReportCaller(true)
 	for {
 		line, err := l.Readline()
 		if err == readline.ErrInterrupt {
@@ -138,102 +139,4 @@ func shellcmd(cmd *cobra.Command, args []string) {
 	}
 exit:
 	destory()
-}
-
-func adCmdHandler(line string) {
-	switch {
-	case line == "add single user":
-		disname, username, org, pwd, des, disabled := getUserInfo()
-		err := ad.AddUser(disname, username, org, pwd, des, disabled)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	case strings.HasPrefix(line, "add user from "):
-		// TODO: 检查文件路径合法性
-		l := line[14:]
-		if len(l) == 0 {
-			println("请输入文件路径")
-			return
-		}
-		for _, e := range ad.AddUserMultiple(l, getOuPath(), false).Errors {
-			fmt.Println(e)
-		}
-	case strings.HasPrefix(line, "del user with "):
-		l := line[14:]
-		if len(l) == 0 {
-
-		}
-	case strings.HasPrefix(line, "query info "):
-		l := ""
-		if l = line[11:]; l == "all" {
-			l = "*"
-		}
-		res, err := ad.GetUserInfoTable(l)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Println(res)
-
-	case strings.HasPrefix(line, "dis ") || strings.HasPrefix(line, "ena "):
-		l := line[4:]
-		if len(l) != 0 {
-			l = strings.TrimSpace(l)
-			err := changeStatus(l, line[:3] == "dis")
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-		}
-	case strings.HasPrefix(line, "del user "):
-		if c := line[9:]; len(c) != 0 {
-			// TODO: search user and list them
-
-			// remove user
-		}
-	case line == "go ad":
-		fmt.Println("connect to ad server...")
-	case line == "re con ad":
-		fmt.Println("reconnect to ad server...")
-	case len(line) == 0:
-	default:
-		fmt.Printf(cmdNotFound, line)
-	}
-}
-
-func zbxCmdHandler(line string) {
-	switch {
-	case strings.HasPrefix(line, "list "):
-		switch line[5:] {
-		case "host":
-			cmdMap[line[:4]]("", line[5:])
-		case "group":
-			cmdMap[line[:4]]("", line[5:])
-		}
-	// query [host] by [key]
-	case strings.HasPrefix(line, "query "):
-		subcmd := line[6:]
-		subList := strings.Split(subcmd, " ")
-		if len(subList) >= 3 {
-			if subList[1] == "by" {
-				cmdMap[line[:5]](subList[2], subList[0])
-			}
-		}
-	case strings.HasPrefix(line, "cfg "):
-		subcmd := line[4:]
-		subList := strings.Split(subcmd, " ")
-		if len(subList) >= 2 {
-			if subList[0] == "export" {
-				cmdMap[line[:3]](subList[1], "")
-			}
-		}
-	case line == "go zbx":
-		println("connect to zabbix server...")
-	case line == "re con zbx":
-		println("reconnect to zabbix server...")
-	case len(line) == 0:
-	default:
-		fmt.Printf(cmdNotFound, line)
-	}
 }
--- cmd/omcmd/zbx.go
+++ cmd/omcmd/zbx.go
@@ -1,1 +1,42 @@
 package cmd
+
+import (
+	"fmt"
+	"strings"
+)
+
+func zbxCmdHandler(line string) {
+	switch {
+	case strings.HasPrefix(line, "list "):
+		switch line[5:] {
+		case "host":
+			cmdMap[line[:4]]("", line[5:])
+		case "group":
+			cmdMap[line[:4]]("", line[5:])
+		}
+	// query [host] by [key]
+	case strings.HasPrefix(line, "query "):
+		subcmd := line[6:]
+		subList := strings.Split(subcmd, " ")
+		if len(subList) >= 3 {
+			if subList[1] == "by" {
+				cmdMap[line[:5]](subList[2], subList[0])
+			}
+		}
+	case strings.HasPrefix(line, "cfg "):
+		subcmd := line[4:]
+		subList := strings.Split(subcmd, " ")
+		if len(subList) >= 2 {
+			if subList[0] == "export" {
+				cmdMap[line[:3]](subList[1], "")
+			}
+		}
+	case line == "go zbx":
+		fmt.Println("connect to zabbix server...")
+	case line == "re con zbx":
+		fmt.Println("reconnect to zabbix server...")
+	case len(line) == 0:
+	default:
+		fmt.Printf(cmdNotFound, line)
+	}
+}
