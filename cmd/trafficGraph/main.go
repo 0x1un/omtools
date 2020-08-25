067cb0273ee03aa1ef30be3c7cd0ac33a18bbb05
0x1un <aumujun@gmail.com>
2020-08-25 19:42:10 +0800

    v0.0.2

--- cmd/trafficGraph/main.go
+++ cmd/trafficGraph/main.go
@@ -20,15 +20,14 @@ func main() {
 		os.Exit(1)
 	}
 	// create graph directory if it doesn't exist
-	path := "./graph/"
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		err := os.Mkdir(path, 0777)
+	graphPrefix := "./graph/"
+	if _, err := os.Stat(graphPrefix); os.IsNotExist(err) {
+		err := os.Mkdir(graphPrefix, 0777)
 		if err != nil {
 			logrus.Fatal(err)
 		}
 	}
 	general := cfg.Section("GENERAL")
-	graphs := cfg.Section("GRAPH")
 	session := g.NewSeesion(
 		general.Key("ZBX_HOST").String()+general.Key("ZBX_PORT").String(),
 		general.Key("ZBX_USERNAME").String(),
@@ -37,21 +36,33 @@ func main() {
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	wg.Add(len(graphs.KeysHash()))
-	for name, graphid := range graphs.KeysHash() {
-		go func(id, name string) {
-			data, err := session.DownloadTrafficGraph(id, general.Key("TIME_FROM").String(), general.Key("TIME_TO").String())
+	for _, section := range cfg.Sections() {
+		if section.Name() == "Default" || len(section.KeysHash()) == 0 || section.Name() == "GENERAL" {
+			continue
+		}
+		graphLocalPath := graphPrefix + section.Name()
+		if _, err := os.Stat(graphLocalPath); os.IsNotExist(err) {
+			err := os.Mkdir(graphLocalPath, 0777)
 			if err != nil {
 				logrus.Fatal(err)
 			}
-			err = ioutil.WriteFile(path+name+".png", data, 0644)
-			if err != nil {
-				logrus.Fatal(err)
-			}
-			wg.Done()
-		}(graphid, name)
+		}
+		wg.Add(len(section.KeysHash()))
+		for name, graphid := range section.KeysHash() {
+			go func(n, g string) {
+				data, err := session.DownloadTrafficGraph(g, general.Key("TIME_FROM").String(), general.Key("TIME_TO").String())
+				if err != nil {
+					logrus.Fatal(err)
+				}
+				err = ioutil.WriteFile(graphLocalPath+"/"+n+".png", data, 0644)
+				if err != nil {
+					logrus.Fatal(err)
+				}
+				wg.Done()
+			}(name, graphid)
+		}
+		wg.Wait()
 	}
-	wg.Wait()
 	fmt.Print("Press 'Enter' to continue...")
 	_, err = bufio.NewReader(os.Stdin).ReadBytes('\n')
 	if err != nil {
